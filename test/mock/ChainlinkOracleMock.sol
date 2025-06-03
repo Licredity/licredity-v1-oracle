24110b81ab39cffcbd5f88b4cf08ef7c62db3c46
wangshouh <wongshouhao@outlook.com>
2025-06-04 03:44:18 +0800

    Add peek amount and test

---
+++ test/mock/ChainlinkOracleMock.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AggregatorV3Interface} from "src/interfaces/AggregatorV3Interface.sol";
+
+contract ChainlinkOracleMock is AggregatorV3Interface {
+    uint8 public decimals;
+    string public description;
+    uint256 public constant version = 1;
+    int256 public answer;
+
+    constructor(uint8 decimals_, string memory description_) {
+        decimals = decimals_;
+        description = description_;
+    }
+
+    function setAnswer(int256 answer_) public {
+        answer = answer_;
+    }
+
+    function latestRoundData() public view override returns (uint80, int256, uint256, uint256, uint80) {
+        return (0, answer, 0, 0, 0);
+    }
+
+    function getRoundData(uint80) public view override returns (uint80, int256, uint256, uint256, uint80) {
+        return (0, answer, 0, 0, 0);
+    }
+}
---
+++ test/mock/DecimalsMock.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DecimalsMock {
+    uint8 public decimals;
+
+    function setDecimals(uint8 _decimals) public {
+        decimals = _decimals;
+    }
+}
