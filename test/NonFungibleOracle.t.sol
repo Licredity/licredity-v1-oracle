64682b2d184fead30e6b84ea4b8f0765bb091cc8
wangshouh <wongshouhao@outlook.com>
2025-06-06 22:54:02 +0800

    Add all position support and fix test

--- test/NonFungibleOracle.t.sol
+++ test/NonFungibleOracle.t.sol
@@ -2,15 +2,19 @@
 pragma solidity ^0.8.20;
 
 import {LicredityChainlinkOracle} from "src/LicredityChainlinkOracle.sol";
+import {Fungible} from "src/types/Fungible.sol";
 import {NonFungible} from "src/types/NonFungible.sol";
 import {Deployers} from "./Deployers.sol";
 import {PoolId} from "v4-core/types/PoolId.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {IPositionManager} from "src/interfaces/IPositionManager.sol";
+import {AggregatorV3Interface} from "src/interfaces/AggregatorV3Interface.sol";
 
 contract NonFungibleOracleTest is Deployers {
     LicredityChainlinkOracle public oracle;
     PoolId public ETHUSDCPoolId;
+    AggregatorV3Interface public constant ZERO_ORACLE = AggregatorV3Interface(address(0));
+    address public constant USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
     function setUp() public {
         deployLicredity();
@@ -35,8 +39,15 @@ contract NonFungibleOracleTest is Deployers {
 
     function test_quoteNonFungible_ETHUSDC() public {
         NonFungible nft = getFungible(23864);
-        // ETH / USDC = 0.984375
+        oracle.updateFeedsConfig(Fungible.wrap(address(0)), ZERO_ORACLE, ZERO_ORACLE);
+        oracle.updateFeedsConfig(
+            Fungible.wrap(USDC),
+            ZERO_ORACLE,
+            AggregatorV3Interface(address(0x5147eA642CAEF7BD9c1265AadcA78f997AbB9649))
+        );
+        // ETH / USDC = 2602.68440965, ETH / pegETH = 0.984375
         uint256 debtTokenAmount = oracle.quoteNonFungible(nft);
-        assertEq(debtTokenAmount, 4903006588562427069110 + 3368008528943);
+        // assertEq(debtTokenAmount, 4903006588562427069110 + 3368008528943);
+        assertApproxEqAbsDecimal(debtTokenAmount, 4826397110616138448896 + 1294051832199103643648, 1e6, 18);
     }
 }
