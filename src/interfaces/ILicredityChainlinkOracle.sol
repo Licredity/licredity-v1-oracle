defb9bf765acfbfd5669c76bb23b639b33f777b5
wangshouh <wongshouhao@outlook.com>
2025-06-12 02:46:06 +0800

    Add interfaces and feeds config struct

--- src/interfaces/ILicredityChainlinkOracle.sol
+++ src/interfaces/ILicredityChainlinkOracle.sol
@@ -2,16 +2,17 @@
 pragma solidity >=0.8.0;
 
 import {AggregatorV3Interface} from "./AggregatorV3Interface.sol";
+import {IOracle} from "./IOracle.sol";
 import {Fungible} from "../types/Fungible.sol";
 import {NonFungible} from "../types/NonFungible.sol";
 import {PoolId} from "v4-core/types/PoolId.sol";
 
-interface ILicredityChainlinkOracle {
+interface ILicredityChainlinkOracle is IOracle {
     /// @notice Event emitted when the new feeds are set
     /// @param asset The address of the asset
     /// @param baseFeed Base feed
     /// @param quoteFeed Quote feed
-    event FeedsUpdated(Fungible indexed asset, AggregatorV3Interface baseFeed, AggregatorV3Interface quoteFeed);
+    event FeedsUpdated(Fungible indexed asset, uint16 mrrBps, AggregatorV3Interface baseFeed, AggregatorV3Interface quoteFeed);
 
     /// @notice Event emitted when the old feeds are deleted
     /// @param asset The address of the asset
@@ -21,26 +22,12 @@ interface ILicredityChainlinkOracle {
     /// @param id The pool ID
     event PoolIdWhitelistUpdated(PoolId indexed id, bool enabled);
 
-    /// @notice Function to get the value, in debt token terms, of some amount of fungible
-    /// @param fungible The fungible to quote
-    /// @param amount The amount of fungible to quote
-    /// @return value The value of the fungible in debt token terms
-    function quoteFungible(Fungible fungible, uint256 amount) external returns (uint256 value);
-
-    /// @notice Function to get the value, in debt token terms, of a non-fungible
-    /// @param nonFungible The non-fungible to quote
-    /// @return value The value of the non-fungible in debt token terms
-    function quoteNonFungible(NonFungible nonFungible) external returns (uint256 value);
-
-    /// @notice Update the price of debt token based on EMA using the price from beforeSwap Hook
-    function update() external;
-
     /// @notice Provides multiple oracle configurations for asset to debt token price calculations
     /// @param asset The address of the asset
     /// @param baseFeed Base feed. Pass address zero if the price = 1
     /// @param quoteFeed Quote feed. Pass address zero if the price = 1
     /// @dev The implementation automatically multiplies the base fee calculation result by the debtToken/token price.
-    function updateFungibleFeedsConfig(Fungible asset, AggregatorV3Interface baseFeed, AggregatorV3Interface quoteFeed)
+    function updateFungibleFeedsConfig(Fungible asset, uint16 mrrBps, AggregatorV3Interface baseFeed, AggregatorV3Interface quoteFeed)
         external;
 
     /// @notice Delete the oracle configuration
--- src/libraries/FeedsConfig.sol
+++ src/libraries/FeedsConfig.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import {AggregatorV3Interface} from "../interfaces/AggregatorV3Interface.sol";
 
 struct FeedsConfig {
+    /// @notice The margin requirement in basis points of asset
+    uint16 mrrBps;
     uint256 scaleFactor;
     AggregatorV3Interface baseFeed;
     AggregatorV3Interface quoteFeed;
