064a58065371e889164d386f16e9ea8aa3228bac
wangshouh <wongshouhao@outlook.com>
2025-06-12 02:46:06 +0800

    Add interfaces and feeds config struct

--- src/interfaces/IOracle.sol
+++ src/interfaces/IOracle.sol
@@ -7,16 +7,24 @@ import {NonFungible} from "../types/NonFungible.sol";
 /// @title IOracle
 /// @notice Interface for the oracle contracts
 interface IOracle {
-    /// @notice Function to get the value, in debt token terms, of some amount of fungible
+    /// @notice Function to get the value and margin requirement, in debt token terms, of some amount of fungible
     /// @param fungible The fungible to quote
     /// @param amount The amount of fungible to quote
     /// @return value The value of the fungible in debt token terms
-    function quoteFungible(Fungible fungible, uint256 amount) external view returns (uint256 value);
+    /// @return marginRequirement The margin requirement in debt token terms
+    function quoteFungible(Fungible fungible, uint256 amount)
+        external
+        view
+        returns (uint256 value, uint256 marginRequirement);
 
-    /// @notice Function to get the value, in debt token terms, of a non-fungible
+    /// @notice Function to get the value and margin requirement, in debt token terms, of a non-fungible
     /// @param nonFungible The non-fungible to quote
     /// @return value The value of the non-fungible in debt token terms
-    function quoteNonFungible(NonFungible nonFungible) external view returns (uint256 value);
+    /// @return marginRequirement The margin requirement in debt token terms
+    function quoteNonFungible(NonFungible nonFungible)
+        external
+        view
+        returns (uint256 value, uint256 marginRequirement);
 
     /// @notice Function to notify the oracle of a price update
     function update() external;
