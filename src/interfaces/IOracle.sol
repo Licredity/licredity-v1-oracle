0221696ece113bb19678b170fdd9158d920d16bb
wangshouh <wongshouhao@outlook.com>
2025-06-12 16:53:57 +0800

    Fix oracle interfaces

--- src/interfaces/IOracle.sol
+++ src/interfaces/IOracle.sol
@@ -7,21 +7,26 @@ import {NonFungible} from "../types/NonFungible.sol";
 /// @title IOracle
 /// @notice Interface for the oracle contracts
 interface IOracle {
+    /// @notice Gets the price of the base fungible in debt fungible terms
+    /// @return uint256 The price of the base fungible in debt fungible terms
+    function getBasePrice() external view returns (uint256);
+
     /// @notice Function to get the value and margin requirement, in debt token terms, of some amount of fungible
-    /// @param fungible The fungible to quote
+    /// @param token The address of the fungible to quote
     /// @param amount The amount of fungible to quote
     /// @return value The value of the fungible in debt token terms
     /// @return marginRequirement The margin requirement in debt token terms
-    function quoteFungible(Fungible fungible, uint256 amount)
+    function quoteFungible(address token, uint256 amount)
         external
         view
         returns (uint256 value, uint256 marginRequirement);
 
     /// @notice Function to get the value and margin requirement, in debt token terms, of a non-fungible
-    /// @param nonFungible The non-fungible to quote
+    /// @param token The non-fungible token to quote
+    /// @param id The ID of the non-fungible token to quote
     /// @return value The value of the non-fungible in debt token terms
     /// @return marginRequirement The margin requirement in debt token terms
-    function quoteNonFungible(NonFungible nonFungible)
+    function quoteNonFungible(address token, uint256 id)
         external
         view
         returns (uint256 value, uint256 marginRequirement);
