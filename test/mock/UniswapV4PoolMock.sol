a4d7bc2d3a129671716abef19d9e7f37bd6bcd23
wangshouh <wongshouhao@outlook.com>
2025-06-24 23:01:06 +0800

    Add position value test

--- test/mock/UniswapV4PoolMock.sol
+++ test/mock/UniswapV4PoolMock.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import {PoolId} from "v4-core/types/PoolId.sol";
 import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
 import {Extsload} from "v4-core/Extsload.sol";
-import {console} from "forge-std/console.sol";
 
 contract UniswapV4PoolMock is Extsload {
     function setPoolIdSqrtPriceX96(PoolId poolId, uint160 sqrtPriceX96) public {
